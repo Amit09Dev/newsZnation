da0066d017a3abb7e4d3e0eaa5b22cffff112b72
freakamy <freak.amit1@gmail.com>
2023-07-24 08:10:04 +0530

    added next and previous buttons

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -4,15 +4,15 @@ import PropTypes from 'prop-types'
 export default class NewsItem extends Component{
 
     render(){
-  let {title, description, imageUrl, NewsUrl} = this.props
+  let {title, description, imageUrl, newsUrl} = this.props
         return(
             <div>
-               <div className="card" style={{width: '18rem'}}>
+               <div className="card mt-3" style={{width: '18rem'}}>
                 <img src={imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
-                    <a href={NewsUrl} target="_blank" rel="noreferrer" className="btn btn-primary btn-sm">Read More</a>
+                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-dark btn-sm">Read More</a>
                 </div>
                 </div>
             </div>
@@ -24,6 +24,6 @@ NewsItem.propTypes = {
     title: PropTypes.string,
     description: PropTypes.string,
     imageUrl : PropTypes.string,
-    NewsUrl : PropTypes.string
+    newsUrl : PropTypes.string
 }
 
